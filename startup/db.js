6407e0d232f8198379a65dd6558fec0094ce04ae
KellyEgesa <kelly.egesa@gmail.com>
2020-08-18 18:05:38 +0300

    we about to do this shit again

--- startup/db.js
+++ startup/db.js
@@ -4,9 +4,12 @@ const config = require("config");
 module.exports = function () {
   const db = config.get("db");
   mongoose
-    .connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
+    .connect(
+      `mongodb+srv://KellyEgesa:led4Agriculture1007@cluster0.g8jcv.mongodb.net/<dbname>?retryWrites=true&w=majority`,
+      { useNewUrlParser: true, useUnifiedTopology: true }
+    )
     .then(() => {
-      console.log(`Connected to the database ${db}`);
+      console.log(`Connected to the database`);
     });
   mongoose.set("useFindAndModify", false);
   mongoose.set("useCreateIndex", true);
