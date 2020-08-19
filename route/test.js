322acf3a32cf751cdd41c6cdc3cbf2462fc32ecf
KellyEgesa <kelly.egesa@gmail.com>
2020-08-19 20:44:05 +0300

    And another one

--- route/test.js
+++ route/test.js
@@ -23,7 +23,7 @@ router.get("/:id", async (req, res) => {
   if (!modules) return res.status(404).send("Module not found");
 
   const result = await Question.find({ modules: modules });
-  res.send(result);
+  res.send(result).select("-modules");
 });
 
 router.post("/", async (req, res) => {
