7fa7d8f998cfe6c98634daf59516f20ceabf1a78
KellyEgesa <kelly.egesa@gmail.com>
2020-08-24 16:46:26 +0300

    Here we go again

--- route/user.js
+++ route/user.js
@@ -60,10 +60,14 @@ router.post("/add", async (req, res) => {
     lastname: req.body.lastname,
     email: req.body.email,
     password: req.body.password,
+    delTime: Date.now(),
   });
 
   const salt = await bcrypt.genSalt(10);
   user.password = await bcrypt.hash(user.password, salt);
+
+  const saved = await user.save();
+  const confirmurl = config.get("front-end") + "confirm/" + saved;
 
   const html = () => {
     return `<body style="padding: 2%;""><div style="width: 85%;
@@ -72,7 +76,7 @@ router.post("/add", async (req, res) => {
     display: flex;
     border: 1px solid black; 
     align-items: center;">
-    <p>Hi ${user.firstname} ${user.lastname}!<br>Thanks for joining LED4Agriculture! Before we get started, we need you to verify your email to make sure we got it right.<br><a href="https://www.facebook.com"><button style="background-color: blue; 
+    <p>Hi ${user.firstname} ${user.lastname}!<br>Thanks for joining LED4Agriculture! Before we get started, we need you to verify your email to make sure we got it right.<br><a href=${confirmurl}><button style="background-color: blue; 
     border: none;
     color: white;
     padding: 20px; 
@@ -88,7 +92,7 @@ router.post("/add", async (req, res) => {
   };
   const subject = "REF: Confirmation Message After Registration";
   const text = "";
-  res.send(await user.save());
+  res.send("success");
   email(req.body.email, subject, text, html());
   //   const token = user.generateAuthToken();
   //   res.header("x-auth-token", token).send(_.pick(user, ["_id", "name"]));
