8ca4e37b97799c9cd9882a5164597e0bfb37a6af
KellyEgesa <kelly.egesa@gmail.com>
2020-08-26 22:48:01 +0300

    sas1

--- route/user.js
+++ route/user.js
@@ -144,11 +144,10 @@ router.post("/forgotPassword", async (req, res) => {
 
 router.get("/reset", async (req, res) => {
   let user = await User.findOne({
-    // resetPasswordExpires: { $gt: Date.now() },
-    resetPasswordToken: req.query.resetPasswordToken,
+    resetPasswordExpires: { $gt: Date.now() },
   });
   // console.log(req.query.resetPasswordToken);
-  if (!user)
+  if (!user || user.resetPasswordToken != req.query.resetPasswordToken)
     return res.status(400).send("Password link is invalid or has expired");
   console.log(user);
   res.status(200).send({ message: "Valid password link" });
