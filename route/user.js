f612740bc49c640fb247a9c9191ecdba6e645cf9
KellyEgesa <kelly.egesa@gmail.com>
2020-08-26 22:18:28 +0300

    sas1

--- route/user.js
+++ route/user.js
@@ -150,7 +150,7 @@ router.get("/reset", async (req, res) => {
   if (!user)
     return res.status(400).send("Password link is invalid or has expired");
 
-  res.status(200).send({ user, message: "Valid password link" });
+  res.status(200).send({ message: "Valid password link" }, user.email);
 });
 
 router.put("/updatePasswordViaEmail", async (req, res) => {
