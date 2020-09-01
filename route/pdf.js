7b9197e5d6a6db857a4c620a0a5fd4bbf980fe8a
KellyEgesa <kelly.egesa@gmail.com>
2020-09-01 09:03:32 +0300

    Added

--- route/pdf.js
+++ route/pdf.js
@@ -42,7 +42,7 @@ router.post("/upload", upload, [auth, editor], function (req, res) {
     error.httpStatusCode = 400;
     return res.status(400);
   }
-  return res.send(file);
+  return res.send(file.originalname).status(200);
 });
 
 router.get("/load/:filename", (req, res) => {
